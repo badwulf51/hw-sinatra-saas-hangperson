5d7ccdc204a3ff8baa89606775552fd4df23865e
badwulf51 <aronomalley@yahoo.com>
2019-08-09 20:26:49 +0100

    all test cases passing

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,16 +1,47 @@
 class HangpersonGame
 
-  attr_accessor :word , :guesses , :wrong_guesses
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses, :check_win_or_lose
+  
   
   def initialize(word)
     @word = word
     @guesses = ''
     @wrong_guesses = ''
+    @word_with_guesses = ''
+    word.each_char do |i|
+      @word_with_guesses << '-'
+    end
+    @check_win_or_lose = :play
   end
 
-  # You can test it by running $ bundle exec irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
-  #  => "cooking"   <-- some random word
+  def guess(letter)
+    raise ArgumentError if letter.nil?
+    raise ArgumentError if letter == ''
+    raise ArgumentError if !letter.match(/[a-zA-Z]/)
+    letter.downcase!
+    if word.include? letter
+      unless guesses.include? letter
+        guesses << letter
+        for i in 0..word.length
+          if word[i] == letter
+            word_with_guesses[i] = letter
+            @check_win_or_lose = :win if !word_with_guesses.include? '-'
+          end
+        end
+        return true
+      end
+    else
+      unless wrong_guesses.include? letter
+        wrong_guesses << letter
+        if wrong_guesses.size >= 7
+          @check_win_or_lose = :lose
+        end
+        return true
+      end
+    end
+    return false
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
@@ -18,15 +49,6 @@ class HangpersonGame
     Net::HTTP.new('watchout4snakes.com').start { |http|
       return http.post(uri, "").body
     }
-  end
-
-  def guess(letter)
-   
-    if @word.include? letter
-      @guesses << letter
-    else
-     @wrong_guesses << letter
-    end
   end
 
 end
