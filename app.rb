c1ec2421583395e422759d4a4525ff12c4b370f2
badwulf51 <aronomalley@yahoo.com>
2019-08-19 17:03:41 +0100

    trying to set up heroku app

--- app.rb
+++ app.rb
@@ -40,6 +40,21 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      if !@game.guess(letter)
+        flash[:message] = "You have already used that letter"
+      end
+      if @game.check_win_or_lose == :win
+        redirect '/win'
+      elsif @game.check_win_or_lose == :lose
+        redirect '/lose'
+      else 
+        redirect '/show'
+      end
+    rescue ArgumentError
+      flash[:message] = "Your input was invalid"
+      redirect '/show'
+    end
     redirect '/show'
   end
   
